b9d8e0a78eeec3858cd8b5c2f5fcc9f36813b728
Iacopo Schianchi <iacopo.schianchi@gmail.com>
2024-09-22 15:05:10 -0400

    Added typing hero text

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react';
+import './Hero.css';
+
+export default function Hero() {
+  const finalLines = ["Hi, I'm", 'Iacopo Schianchi'];
+  const [lines, setLines] = useState(['', '']);
+  const [finished, setFinished] = useState([false, false]);
+
+  async function writeLine(lineIndex) {
+    return new Promise((resolve) => {
+      let i = 0;
+
+      let typingInterval = setInterval(() => {
+        const currentLine = finalLines[lineIndex];
+        const newChar = currentLine[i];
+
+        setLines((prevLines) => {
+          return prevLines.map((prevLine, index) =>
+            index === lineIndex ? prevLine.concat(newChar) : prevLine
+          );
+        });
+
+        i++;
+
+        // Check if we're at the end of "Hi,"
+        if (lineIndex === 0 && currentLine.slice(0, i) === 'Hi,') {
+          clearInterval(typingInterval);
+
+          // Pause for 1 second at "Hi,"
+          setTimeout(() => {
+            typingInterval = setInterval(() => {
+              const nextChar = currentLine[i];
+              setLines((prevLines) => {
+                return prevLines.map((prevLine, index) =>
+                  index === lineIndex ? prevLine.concat(nextChar) : prevLine
+                );
+              });
+
+              i++;
+              if (i >= currentLine.length) {
+                clearInterval(typingInterval);
+                setFinished((prevFinished) =>
+                  prevFinished.map((value, index) =>
+                    index === lineIndex ? true : value
+                  )
+                );
+                resolve();
+              }
+            }, 200);
+          }, 1000);
+        }
+
+        if (i >= currentLine.length) {
+          clearInterval(typingInterval);
+          setTimeout(
+            () => {
+              setFinished((prevFinished) =>
+                prevFinished.map((value, index) =>
+                  index === lineIndex ? true : value
+                )
+              );
+            },
+            lineIndex === 1 ? 3000 : 0
+          );
+          resolve();
+        }
+      }, 200);
+    });
+  }
+
+  useEffect(() => {
+    async function writeHeader() {
+      await writeLine(0);
+      await writeLine(1);
+    }
+
+    writeHeader();
+  }, []);
+
+  return (
+    <div className="hero">
+      <h1 className={`title ${finished[0] ? 'finished' : ''}`} id="typing-text">
+        {lines[0]}
+      </h1>
+      <br />
+      <h1 className={`title ${finished[1] ? 'finished' : ''}`} id="typing-text">
+        {lines[1]}
+      </h1>
+    </div>
+  );
+}
